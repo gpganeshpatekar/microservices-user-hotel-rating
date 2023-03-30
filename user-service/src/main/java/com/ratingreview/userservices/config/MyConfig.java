1b0472ad55324a9da73e689e6cc3dd7cd9b7b652
gpganeshpatekar <gpganeshpatekar@gmail.com>
2023-03-30 13:07:31 +0530

    configuration done to use service name in the url. @LoadBalanced used to balance the load

--- user-service/src/main/java/com/ratingreview/userservices/config/MyConfig.java
+++ user-service/src/main/java/com/ratingreview/userservices/config/MyConfig.java
@@ -1,6 +1,7 @@
 package com.ratingreview.userservices.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ public class MyConfig {
 	}
 	
 	@Bean
+	@LoadBalanced // this annotation will balance the load and you can use service name instead of port no in the url
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
